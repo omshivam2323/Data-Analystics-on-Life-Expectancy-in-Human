d7b7431071b51de09d555daeff7f9caf10ca8dec
omshivam2323 <shivam2323mishra@gmail.com>
2021-05-06 17:28:37 +0530

    initialized

---
+++ AnalyseData.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+class Analyse:
+
+    def __init__(self, path = 'datasets/apps.csv'):
+        self.df = pd.read_csv(path)
+        self.cleanData()
+
+    def cleanData(self):
+        self.df.drop(columns=[self.df.columns[0]], inplace=True)
+
+    def getDataframe(self):
+        return self.df
+
+    def getCategories(self):
+        return self.df.groupby('Category').count().sort_values('App')['App'][::-1]
+
+    
---
+++ app.py
@@ -0,0 +1,92 @@
+import streamlit as st
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import pandas as pd
+from database import Report
+from visualization import plot, plotBar
+from AnalyseData import Analyse
+
+engine = create_engine('sqlite:///db.sqlite3')
+Session = sessionmaker(bind=engine)
+sess = Session()
+
+analysis = Analyse()
+
+st.title('Global Warming and Climate Change Analysis')
+sidebar = st.sidebar
+
+
+def viewDataset():
+    st.header('Data Used in Project')
+    dataframe = analysis.getDataframe()
+
+    with st.spinner("Loading Data..."):
+        st.dataframe(dataframe)
+
+        st.markdown('---')
+        cols = st.beta_columns(4)
+        cols[0].markdown("### No. of Rows :")
+        cols[1].markdown(f"# {dataframe.shape[0]}")
+        cols[2].markdown("### No. of Columns :")
+        cols[3].markdown(f"# {dataframe.shape[1]}")
+        st.markdown('---')
+
+        st.header('Summary')
+        st.dataframe(dataframe.describe())
+        st.markdown('---')
+
+        types = {'object' : 'Categorical', 'int64': 'Numerical', 'float64': 'Numerical'}
+        types = list(map(lambda t: types[str(t)], dataframe.dtypes))
+        st.header('Dataset Columns')
+        for col, t in zip(dataframe.columns, types):
+            st.markdown(f"### {col}")
+            cols = st.beta_columns(4)
+            cols[0].markdown('#### Unique Values :')
+            cols[1].markdown(f"# {dataframe[col].unique().size}")
+            cols[2].markdown('#### Type :')
+            cols[3].markdown(f"## {t}")
+
+
+
+def viewForm():
+
+    st.plotly_chart(plot())
+
+    title = st.text_input("Report Title")
+    desc = st.text_area('Report Description')
+    btn = st.button("Submit")
+
+    if btn:
+        report1 = Report(title = title, desc = desc, data = "")
+        sess.add(report1)
+        sess.commit()
+        st.success('Report Saved')
+
+def analyse():
+    data = analysis.getCategories()
+    st.plotly_chart(plotBar(data.index, data.values))
+
+def viewReport():
+    reports = sess.query(Report).all()
+    titlesList = [ report.title for report in reports ]
+    selReport = st.selectbox(options = titlesList, label="Select Report")
+    
+    reportToView = sess.query(Report).filter_by(title = selReport).first()
+
+    markdown = f"""
+        ## {reportToView.title}
+        ### {reportToView.desc}
+        
+    """
+
+    st.markdown(markdown)
+
+sidebar.header('Choose Your Option')
+options = [ 'View Dataset', 'Analyse', 'View Report' ]
+choice = sidebar.selectbox( options = options, label="Choose Action" )
+
+if choice == options[0]:
+    viewDataset()
+elif choice == options[1]:
+    analyseResponses()    
