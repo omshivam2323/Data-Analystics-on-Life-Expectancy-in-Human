9cd9a8798771305b58c477b39517673a3da9af43
SHIVAM MISHRA <79130468+omshivam2323@users.noreply.github.com>
2021-06-02 08:31:07 +0530

    /

--- visualization.py
+++ visualization.py
@@ -6,6 +6,7 @@ def plotBar(datapoints, title, xlabel, ylabel):
     layout = go.Layout(title=title,
                        xaxis=dict(title=xlabel),
                        yaxis=dict(title=ylabel))
+  
     fig = go.Figure(layout=layout)
 
     fig.add_trace(go.Bar(x=datapoints.index, y=datapoints.values.flatten()))
